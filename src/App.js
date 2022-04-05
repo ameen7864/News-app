909f80d6f141a146c6b3b5ef03329a443d327d24
Ameen Ahmed Shariff <93003469+ameen7864@users.noreply.github.com>
2022-04-05 16:44:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './Components/Navbar';
 import News from './Components/News';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
 
-} from "react-router-dom";
 export default class App extends Component {
   render() {
     return (
